283fabc1e2d4c7869f7a78a8e42680c784467012
Aimee <aimeehorwood@hotmail.com>
2023-01-23 16:55:59 +0000

    Adds second test for transactions class to initialize with an empty array

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,10 +1,15 @@
 class Transactions
   def initialize
     @date = "23/01/2023"
+    @transactions_list = []
   end
 
   def return_date
     @date
   end
+
+  def return_all_transactions
+    @transactions_list
+  end 
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Transactions do
     transactions = Transactions.new
     expect(transactions.return_date).to eq "23/01/2023"
   end
+
+  it "initializes with an empty array of transactions" do 
+    transactions = Transactions.new 
+    expect(transactions.return_all_transactions).to eq []
+  end 
 end
