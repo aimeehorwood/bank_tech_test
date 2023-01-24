ffeed1ec604455c35998a2a3f04b3e2e43b24e3b
Aimee <aimeehorwood@hotmail.com>
2023-01-24 14:38:10 +0000

    Adds test for credit_account method integration

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,13 +1,24 @@
 require "bank_account"
 require "transactions"
 
-
 RSpec.describe "bank account and transactions integration" do 
-    it "can add transaction and views transaction history" do 
+    it "can add transaction through #debit_account method and views transaction history" do 
         bank_account = BankAccount.new
         transaction = Transactions.new
         transaction.debit_account(500)
         bank_account.add_transaction(transaction)
         expect(bank_account.view_history).to eq [["24/01/2023", 0, 500]]
     end
+
+    it "can add transaction through #credit_account method and views transaction history" do 
+        bank_account = BankAccount.new
+        transaction = Transactions.new
+        transaction.credit_account(500)
+        bank_account.add_transaction(transaction)
+        expect(bank_account.view_history).to eq [["24/01/2023", 500, 0]]
+    end
+
+
+
+
 end
