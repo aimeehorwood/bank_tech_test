ecaf854e0bae74d12f9ab0e40c9e6430d9be9c3e
Aimee <aimeehorwood@hotmail.com>
2023-01-25 14:50:43 +0000

    adds tests for updating the bank account balance when credit and debited

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -14,10 +14,23 @@ RSpec.describe BankAccount do
         expect(bank_account.all_transactions.first.credit).to eq 100
     end 
 
+    it "can credit the bank account and update the balance to the amount" do
+        bank_account = BankAccount.new
+        bank_account.credit(100,"25/01/2023")
+        expect(bank_account.all_transactions.first.balance).to eq -100
+    end 
+
+
     it "can debit the bank account and return the amount" do 
         bank_account = BankAccount.new
         bank_account.debit(55.40,"25/01/2023")
         expect(bank_account.all_transactions.first.debit).to eq 55.40
     end  
+
+    it "can debit the bank account and update the balance to the amount" do
+        bank_account = BankAccount.new
+        bank_account.debit(55.40,"25/01/2023")
+        expect(bank_account.all_transactions.first.balance).to eq 55.40
+    end 
 
 end
