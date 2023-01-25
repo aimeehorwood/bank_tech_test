b4006ec332f9da6af7e9074321c8f027ad7180cc
Aimee <aimeehorwood@hotmail.com>
2023-01-25 14:54:47 +0000

    Adds more tests for credit and debit methods on bank account class

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -10,14 +10,16 @@ RSpec.describe BankAccount do
 
     it "can credit the bank account and return the amount" do
         bank_account = BankAccount.new
-        bank_account.credit(100,"25/01/2023")
+        bank_account.credit(100,"23/01/2023")
         expect(bank_account.all_transactions.first.credit).to eq 100
+        expect(bank_account.all_transactions.first.date).to eq "23/01/2023"
     end 
 
     it "can credit the bank account and update the balance to the amount" do
         bank_account = BankAccount.new
-        bank_account.credit(100,"25/01/2023")
+        bank_account.credit(100,"23/01/2023")
         expect(bank_account.all_transactions.first.balance).to eq -100
+        expect(bank_account.all_transactions.first.date).to eq "23/01/2023"
     end 
 
 
@@ -25,12 +27,14 @@ RSpec.describe BankAccount do
         bank_account = BankAccount.new
         bank_account.debit(55.40,"25/01/2023")
         expect(bank_account.all_transactions.first.debit).to eq 55.40
+        expect(bank_account.all_transactions.first.date).to eq "25/01/2023"
     end  
 
     it "can debit the bank account and update the balance to the amount" do
         bank_account = BankAccount.new
         bank_account.debit(55.40,"25/01/2023")
         expect(bank_account.all_transactions.first.balance).to eq 55.40
+        expect(bank_account.all_transactions.first.date).to eq "25/01/2023"
     end 
-
+    
 end
