d08c102d4ffefe8471af99e70bfe454544a209a6
Aimee <aimeehorwood@hotmail.com>
2023-01-25 14:26:56 +0000

    new approach: first test for bank_account for all_transactions method

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,10 +1,10 @@
-require "bank_account"
+require "../lib/bank_account"
 
 RSpec.describe BankAccount do
     context "when no transactions added" do
         it "initalially has an empty list" do 
             bank_account = BankAccount.new 
-            expect(bank_account.view_history).to eq []
+            expect(bank_account.all_transactions).to eq []
         end
     end
    
