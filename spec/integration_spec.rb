d6e2000af14273ed8953f5acb890d669b7f67e22
Aimee <aimeehorwood@hotmail.com>
2023-01-25 14:06:36 +0000

    new approach to problem: changes code and tests for transaction class

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,34 +1,31 @@
 require "bank_account"
 require "transactions"
 
-RSpec.describe "bank account and transactions integration" do 
-    it "can add transaction through #debit_account method and return transaction history with - balance" do 
-        bank_account = BankAccount.new
-        transaction = Transactions.new
-        transaction.debit_account(500)
-        bank_account.add_transaction(transaction)
-        expect(bank_account.view_history).to eq ["24/01/2023 || Credit: 0 || Debit: 500 || Balance: -500"]
-    end
+RSpec.describe "bank account and transactions integration" do
+  it "can add transaction through #debit_account method and return transaction history with - balance" do
+    bank_account = BankAccount.new
+    transaction = Transactions.new
+    transaction.debit_account(500)
+    bank_account.add_transaction(transaction)
+    expect(bank_account.view_history).to eq ["25/01/2023 || Credit: 0 || Debit: 500 || Balance: -500"]
+  end
 
-    it "can add transaction through #credit_account method and return transaction history with + balance" do 
-        bank_account = BankAccount.new
-        transaction = Transactions.new
-        transaction.credit_account(500)
-        bank_account.add_transaction(transaction)
-        expect(bank_account.view_history).to eq ["24/01/2023 || Credit: 500 || Debit: 0 || Balance: 500"]
-    end
+  it "can add transaction through #credit_account method and return transaction history with + balance" do
+    bank_account = BankAccount.new
+    transaction = Transactions.new
+    transaction.credit_account(500)
+    bank_account.add_transaction(transaction)
+    expect(bank_account.view_history).to eq ["25/01/2023 || Credit: 500 || Debit: 0 || Balance: 500"]
+  end
 
-    it "can add two transactions through #credit_account method and views transaction history" do 
-        bank_account = BankAccount.new
-        transaction = Transactions.new
-        transaction_two = Transactions.new 
-        transaction.credit_account(500)
-        transaction_two.credit_account(1000)
-        bank_account.add_transaction(transaction)
-        bank_account.add_transaction(transaction_two)
-        expect(bank_account.view_history).to eq ["24/01/2023 || Credit: 500 || Debit: 0 || Balance: 500", "24/01/2023 || Credit: 1000 || Debit: 0 || Balance: 1500"]
-    end
-
-
-
+  it "can add two transactions through #credit_account method and views transaction history" do
+    bank_account = BankAccount.new
+    transaction = Transactions.new
+    transaction_two = Transactions.new
+    transaction.credit_account(500)
+    transaction_two.credit_account(1000)
+    bank_account.add_transaction(transaction)
+    bank_account.add_transaction(transaction_two)
+    expect(bank_account.view_history).to eq ["25/01/2023 || Credit: 500 || Debit: 0 || Balance: 500", "25/01/2023 || Credit: 1000 || Debit: 0 || Balance: 1500"]
+  end
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,43 +1,23 @@
-require "transactions"
+require "../lib/transactions"
 
 RSpec.describe Transactions do
-  it "initializes with date" do
-    time_one = Time.new(2023, 1, 23)
-    transactions = Transactions.new
-    date = Time.now.strftime("%d/%m/%Y")
-    expect(transactions.return_date).to eq date
+  it "can return date from a single transaction" do
+    transaction = Transactions.new("25/01/2023", 100, 0, 0)
+    expect(transaction.date).to eq("25/01/2023")
   end
 
-  it "initializes with 0 for credit" do
-    transactions = Transactions.new
-    expect(transactions.return_credit).to eq 0
+  it "can return credit from a single transaction" do
+    transaction = Transactions.new("25/01/2023", 100, 0, 0)
+    expect(transaction.credit).to eq 100
   end
 
-  it "initializes with 0 for debit" do
-    transactions = Transactions.new
-    expect(transactions.return_debit).to eq 0
+  it "can return debit from a single transaction" do
+    transaction = Transactions.new("25/01/2023", 0, 10.32, 0)
+    expect(transaction.debit).to eq 10.32
   end
 
-  it "initializes with 0 for balance" do
-    transactions = Transactions.new
-    expect(transactions.return_balance).to eq 0
+  it "can return balance from a single transaction" do
+    transaction = Transactions.new("25/01/2023", 0, 10.32, 10.32)
+    expect(transaction.balance).to eq 10.32
   end
 end
-
-describe "#credit method" do
-  it "updates credit from 0 to given number" do
-    transactions = Transactions.new
-    transactions.credit_account(500)
-    expect(transactions.return_credit).to eq 500
-  end
-end
-
-describe "#debit method" do
-  it "updates debit from 0 to given number" do
-    transactions = Transactions.new
-    transactions.debit_account(500)
-    expect(transactions.return_debit).to eq 500
-  end
-end
-
-
